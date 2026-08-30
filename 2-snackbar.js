aa5b192c6336618d6ea1d6158d057e32dccd06d0
stansokolov9-dot <stansokolov9@gmail.com>
2026-08-30 19:18:50 +0000

    Deploying to gh-pages from @ stansokolov9-dot/goit-js-hw-10@0e6383d08dff139ca1fc2b5c525ed35bc614d9eb 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form");t.addEventListener("submit",i=>{i.preventDefault();const s=Number(t.elements.delay.value),r=t.elements.state.value,m=new Promise((e,l)=>{setTimeout(()=>{r==="fulfilled"?e(s):l(s)},s)});m.then(e=>{o.success({message:`✅ Fulfilled promise in ${e} ms`})}),m.catch(e=>{o.error({message:`❌ Rejected promise in ${e} ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
